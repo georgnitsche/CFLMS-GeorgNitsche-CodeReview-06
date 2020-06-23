39fd4aa244beeb77e2a521f8d7ef8f93e5966356
georgnitsche <georg.nitsche@alumni.uni-graz.at>
2020-06-23 22:00:10 +0200

    erste Version

---
+++ script.js
@@ -0,0 +1,98 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var events = [];
+var sightsArray = [];
+var restaurantsArray = [];
+var loc = /** @class */ (function () {
+    function loc(city, zipcode) {
+        this.city = "";
+        this.zipcode = "";
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+    loc.prototype.showInfo = function () {
+        return "The City is " + this.city + ", Zipcode is " + this.zipcode + ".";
+    };
+    return loc;
+}());
+var loc1 = new loc("Vienna", "1010");
+var loc2 = new loc("Vienna", "1020");
+/*function showloc1(){
+    document.getElementById("properties").innerHTML= loc1.showInfo();
+}
+function showloc2() {
+document.getElementById("properties").innerHTML= loc2.showInfo();
+}
+*/
+var Sights = /** @class */ (function (_super) {
+    __extends(Sights, _super);
+    /*type; km; seats; fuel;*/
+    function Sights(city, zipcode, address, image) {
+        var _this = _super.call(this, city, zipcode) || this;
+        _this.address = address;
+        _this.image = image;
+        sightsArray.push(_this);
+        console.table(sightsArray);
+        return _this;
+    }
+    Sights.prototype.showInfo = function () {
+        return _super.prototype.showInfo.call(this) + " It's a " + this.address + " which has " + this.image + ", has " + this.zipcode + " and fueled with " + this.city + ".<br>";
+    };
+    return Sights;
+}(loc));
+var Restaurants = /** @class */ (function (_super) {
+    __extends(Restaurants, _super);
+    /*type; km; seats; fuel;*/
+    function Restaurants(city, zipcode, address, image, type) {
+        var _this = _super.call(this, city, zipcode) || this;
+        _this.address = address;
+        _this.image = image;
+        _this.type = type;
+        restaurantsArray.push(_this);
+        console.table(restaurantsArray);
+        return _this;
+    }
+    Restaurants.prototype.showInfo = function () {
+        return _super.prototype.showInfo.call(this) + " It's a " + this.address + " which has " + this.image + ", has " + this.zipcode + " and fueled with " + this.city + ".<br>";
+    };
+    return Restaurants;
+}(loc));
+var sights1 = new Sights("Vienna", "1020", "Riesenradplatz 1", "https://www.wienerriesenrad.com/media/lightbox_gallery/stadtgasthaus-eisvogel-in-wien-medium-177.jpg");
+var sights2 = new Sights("Vienna", "1010", "Stephansplatz 3", "https://www.wien.info/media/images/41220-stephansdom-haas-haus-panorama-1to1.jpeg");
+for (var i = 0; i < sightsArray.length; i++) {
+    document.getElementById("properties").innerHTML += '<button type="button" onclick="sightsinfo(' + i + ')">ShowInfo</button><br />';
+}
+var restaurants1 = new Restaurants("Vienna", "1020", "Schachgasse 2", "https://www.wienerriesenrad.com/media/lightbox_gallery/stadtgasthaus-eisvogel-in-wien-medium-177.jpg", "Asian");
+var restaurants2 = new Restaurants("Vienna", "1010", "Friedrich Müller Weg 3", "https://www.wien.info/media/images/41220-stephansdom-haas-haus-panorama-1to1.jpeg", "Asian");
+console.table(restaurantsArray);
+for (var i = 0; i < restaurantsArray.length; i++) {
+    document.getElementById("properties").innerHTML += '<button type="button" onclick="restaurantsinfo(' + i + ')">ShowInfo</button><br />';
+}
+function sightsinfo(x) {
+    var y = sightsArray[x];
+    var city = y.city;
+    var zipcode = y.zipcode;
+    var address = y.address;
+    var image = y.image;
+    document.getElementById("displayproperties").innerHTML += 'Info is' + city + zipcode + address + '<img src=' + image + '>';
+}
+function restaurantsinfo(x) {
+    var city = restaurantsArray[x].city;
+    var zipcode = restaurantsArray[x].zipcode;
+    var address = restaurantsArray[x].address;
+    var image = restaurantsArray[x].image;
+    var type = restaurantsArray[x].type;
+    document.getElementById("displayproperties").innerHTML += 'Info is' + city + zipcode + type + address + '<img src=' + image + '>';
+}
+// ab hier der Restaurants-Array-Code
---
+++ script.ts
@@ -0,0 +1,119 @@
+
+let events:Array<any>=[];
+let sightsArray:Array<any>=[];
+let restaurantsArray:Array<any>=[];
+
+class loc{
+	city="";
+	zipcode="";
+
+
+
+	constructor(city, zipcode){
+		this.city = city;
+		this.zipcode = zipcode;
+	}
+
+	showInfo(){
+		return `The City is ${this.city}, Zipcode is ${this.zipcode}.`;
+	}
+
+}
+
+var loc1 = new loc("Vienna", "1010");
+var loc2 = new loc("Vienna", "1020");
+	
+/*function showloc1(){
+	document.getElementById("properties").innerHTML= loc1.showInfo();
+}
+function showloc2() {
+document.getElementById("properties").innerHTML= loc2.showInfo();
+}
+*/
+
+class Sights extends loc{
+	address; image; 
+	/*type; km; seats; fuel;*/
+
+	constructor(city, zipcode, address, image){
+		super(city, zipcode);
+		this.address = address;
+		this.image = image;
+		sightsArray.push(this);
+		console.table(sightsArray);
+
+	}
+
+	showInfo(){
+		return `${super.showInfo()} It's a ${this.address} which has ${this.image}, has ${this.zipcode} and fueled with ${this.city}.<br>`;
+	}
+
+}
+
+
+class Restaurants extends loc{
+	type;
+	address;
+	image;
+	/*type; km; seats; fuel;*/
+
+	constructor(city, zipcode, address, image, type){
+		super(city, zipcode);
+		this.address = address;
+		this.image = image;
+		this.type = type;
+		restaurantsArray.push(this);
+		console.table(restaurantsArray);
+
+	}
+
+	showInfo(){
+		return `${super.showInfo()} It's a ${this.address} which has ${this.image}, has ${this.zipcode} and fueled with ${this.city}.<br>`;
+	}
+
+}
+
+var sights1 = new Sights("Vienna", "1020", "Riesenradplatz 1", "https://www.wienerriesenrad.com/media/lightbox_gallery/stadtgasthaus-eisvogel-in-wien-medium-177.jpg",);
+var sights2 = new Sights("Vienna", "1010", "Stephansplatz 3", "https://www.wien.info/media/images/41220-stephansdom-haas-haus-panorama-1to1.jpeg",);
+
+
+
+for (let i = 0; i<sightsArray.length; i++) {
+		document.getElementById("properties").innerHTML+= '<button type="button" onclick="sightsinfo(' +i +')">ShowInfo</button><br />';
+	
+}
+
+
+var restaurants1 = new Restaurants("Vienna", "1020", "Schachgasse 2", "https://www.wienerriesenrad.com/media/lightbox_gallery/stadtgasthaus-eisvogel-in-wien-medium-177.jpg", "Asian");
+var restaurants2 = new Restaurants("Vienna", "1010", "Friedrich Müller Weg 3", "https://www.wien.info/media/images/41220-stephansdom-haas-haus-panorama-1to1.jpeg", "Asian");
+
+console.table(restaurantsArray)
+for (let i = 0; i<restaurantsArray.length; i++) {
+		document.getElementById("properties").innerHTML+= '<button type="button" onclick="restaurantsinfo(' +i +')">ShowInfo</button><br />';
+	
+}
+
+
+function sightsinfo(x){
+	let y = sightsArray[x];
+	let city = y.city;
+	let zipcode = y.zipcode;
+	let address = y.address;
+	let image = y.image
+	document.getElementById("displayproperties").innerHTML += 'Info is' +city +zipcode +address +'<img src=' +image+'>';
+	
+}
+
+
+function restaurantsinfo(x) {
+
+	let city = restaurantsArray[x].city;
+	let zipcode = restaurantsArray[x].zipcode;
+	let address = restaurantsArray[x].address;
+	let image = restaurantsArray[x].image;
+	let type = restaurantsArray[x].type;
+	document.getElementById("displayproperties").innerHTML += 'Info is' +city +zipcode +type +address +'<img src=' +image+'>';	
+}
+
+
+// ab hier der Restaurants-Array-Code
